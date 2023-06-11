703bfdfd56561f5176402560427cd2152f105ea2
genie-ru <genie.zgmf987@gmail.com>
2023-06-11 19:44:22 +0900

    add transition

--- src/components/Transition.js
+++ src/components/Transition.js
@@ -1,5 +1,31 @@
+import { useState } from "react";
+import { Transition } from "react-transition-group";
 const TransitionComp = () => {
-  return <div>Transition</div>;
+  let [show, setShow] = useState(true)
+
+  const toggleDiv = () => {
+    setShow(!show ? true : false)
+  }
+
+  return (
+    <>
+      <Transition
+        in={show}
+        timeout={2000}
+      >
+        { state => 
+          <div style={{
+            background: 'red',
+            height: '100px',
+            transition: 'all 2s ease',
+            opacity: state === 'exited' || state === 'exiting' ? 0 : 1
+          }}>
+            {state}
+          </div>}
+      </Transition>
+      <button className="btn btn-primary" onClick={toggleDiv}>Toggle</button>
+    </>
+  );
 };
 
 export default TransitionComp;
